8f0c1c7c77f1e7bbfc0d9a80e6ee30b208b66161
cclab.chen <2504919775@qq.com>
2023-08-18 17:39:25 +0800

    添加新的损失函数组合

--- loss_fn.py
+++ loss_fn.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     import models as m
 
-__all__ = ['DiceCELoss', 'get_loss_fn']
+__all__ = ['DiceCELoss', 'DiceMSELoss', 'DiceFocalLoss', 'get_loss_fn']
 
 
 class DiceCELoss(nn.Module):
@@ -65,11 +65,37 @@ class DiceMSELoss(nn.Module):
         return self.lamb_dice * dice_loss + self.lamb_mse * mse_loss
 
 
+class DiceFocalLoss(nn.Module):
+    def __init__(self, mode: str = 'binary', lamb_dice: float = 1.0, lamb_focal: float = 1.0):
+        """Dice Loss and Focal Loss
+
+                Args
+                    - **mode**: Loss mode 'binary', 'multiclass' or 'multilabel'
+                    - **lamb_dice**: Factor of dice loss
+                    - **lamb_ce**: Factor of focal loss
+
+                Shape
+                    - **y_pred**: torch.Tensor of shape (N, C, H, W)
+                    - **y_true**: torch.Tensor of shape (N, H, W) or (N, C, H, W)
+                """
+        super(DiceFocalLoss, self).__init__()
+        self.lamb_dice = lamb_dice
+        self.lamb_focal = lamb_focal
+        self.dice = m.losses.DiceLoss(mode=mode)
+        self.focal = m.losses.FocalLoss(mode=mode)
+
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        dice_loss = self.dice(y_pred, y_true)
+        focal_loss = self.focal(y_pred, y_true)
+        return self.lamb_dice * dice_loss + self.lamb_focal * focal_loss
+
+
 loss_map = {
-    'dice_ce_loss': DiceCELoss,
     'dice_loss': m.losses.DiceLoss,
     'mse_loss': nn.MSELoss,
+    'dice_ce_loss': DiceCELoss,
     'dice_mse_loss': DiceMSELoss,
+    'dice_focal_loss': DiceFocalLoss,
 }
 
 
